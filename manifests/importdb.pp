7a1818cde3b2a63a1595593a7e3336eedaecf7d2
Carlos Herrera <caherrerac@falabella.cl>
2019-06-25 18:24:04 -0400

    proftpd::importdb fix unless rule

--- manifests/importdb.pp
+++ manifests/importdb.pp
@@ -17,12 +17,17 @@ class proftpd::importdb (
 
   }
 
+  $chk = "sum(DATA_LENGTH+INDEX_LENGTH) as chk"
+  $chktable = "information_schema.TABLES"
+  $mysql_query = "mysql -e \"select $chk from $chktable where TABLE_SCHEMA='$sql_dbname' and TABLE_ROWS > 0;\"  "
+  $unless="[ \"$($mysql_query | sed 1d )\" == \"0\" ] && exit 0 || exit 1"
+
   if $import {
     $mysql = "mysql -h${sql_host} -u${admin_user} -p${admin_pass} ${sql_dbname}"
     exec { "import-$sql_dbname":
       command => "$mysql < $dump",
       path    => '/usr/bin:/usr/sbin:/bin',
-      unless  => "$mysql -e \"show tables\" 2>/dev/null"
+      unless  => $unless
     }
   }
 
