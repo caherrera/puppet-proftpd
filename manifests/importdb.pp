d7aef4fb1148d4a795184a0dd149d7fe2af78c61
Carlos Herrera <caherrerac@falabella.cl>
2019-06-26 10:25:19 -0400

    fix adding credentials in check process

--- manifests/importdb.pp
+++ manifests/importdb.pp
@@ -16,14 +16,14 @@ class proftpd::importdb (
 
 
   }
-
+  $mysql = "mysql -h${sql_host} -u${admin_user} -p${admin_pass} ${sql_dbname}"
   $chk = "sum(DATA_LENGTH+INDEX_LENGTH) as chk"
   $chktable = "information_schema.TABLES"
-  $mysql_query = "mysql -e \"select $chk from $chktable where TABLE_SCHEMA='$sql_dbname' and TABLE_ROWS > 0;\"  "
+  $mysql_query = "$mysql -e \"select $chk from $chktable where TABLE_SCHEMA='$sql_dbname' and TABLE_ROWS > 0;\"  "
   $unless="[ \"$($mysql_query | sed 1d )\" == \"0\" ] && exit 0 || exit 1"
 
   if $import {
-    $mysql = "mysql -h${sql_host} -u${admin_user} -p${admin_pass} ${sql_dbname}"
+
     exec { "import-$sql_dbname":
       command => "$mysql < $dump",
       path    => '/usr/bin:/usr/sbin:/bin',
